a38012f75669906b5e0dd38d5e0bdf47fb2e6172
clarkjoao <clark.joao@gmail.com>
2022-02-25 12:14:14 -0300

    first commit

---
+++ pages/_middleware.ts
@@ -0,0 +1,15 @@
+import type { NextRequest, NextResponse } from "next/server";
+
+// Block Austria, prefer Germany
+const BLOCKED_COUNTRY = ["DE", "US"];
+
+export function middleware(req: NextRequest, res: NextResponse) {
+  const country = req?.geo?.country || "PT";
+  console.log(req);
+  if (BLOCKED_COUNTRY.includes(country)) {
+    return new Response("Blocked for legal reasons", { status: 451 });
+  }
+  return new Response(
+    `Greetings from ${country}, where you are not blocked.${res}`
+  );
+}
